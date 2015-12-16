3bf876664506ca290cd7823a1a7341a889ce7c09
conscou <scoullisconstantina@gmail.com>
2015-12-16 00:30:44 -0500

    cleaned up caffeine.js

--- caffeine.js
+++ caffeine.js
@@ -1,206 +1,66 @@
 var caffeineIntake = 0;
 
+function caffeineCheck(caffeineIntake){
+	if(caffeineIntake >= 10000){
+		document.getElementById("count").innerHTML= caffeineIntake + "mg of caffeine was consumed...how are you not dead?";
+    	$("#count").css("color", "red");
+		
+	} else if(caffeineIntake >= 1000){
+		document.getElementById("count").innerHTML= caffeineIntake + "mg of caffeine was consumed...no seriously stop";
+    	$("#count").css("color", "red");
+	} else if(caffeineIntake >= 400){
+    	document.getElementById("count").innerHTML= caffeineIntake + "mg of caffeine was consumed...but you should stop now";
+    	$("#count").css("color", "red");
+    } else {
+		document.getElementById("count").innerHTML= caffeineIntake + " mg of caffeine was consumed";
+		}
+}
+
+
 function incrementCaffeine (){
-
-
 
 	$("#keurig").click(function () {
 		caffeineIntake = caffeineIntake + 150;
+		caffeineCheck(caffeineIntake);
 		
-		if(caffeineIntake >= 10000){
-		
-		 document.getElementById("count").innerHTML= caffeineIntake + "mg of caffeine was consumed...how are you not dead?";
-    	$("#count").css("color", "red");
-		
-		} else if(caffeineIntake >= 1000){
-		
-		 document.getElementById("count").innerHTML= caffeineIntake + "mg of caffeine was consumed...no seriously stop";
-    	$("#count").css("color", "red");
-		
-		} else if(caffeineIntake >= 400){
-    	
-        document.getElementById("count").innerHTML= caffeineIntake + "mg of caffeine was consumed...but you should stop now";
-    	$("#count").css("color", "red");
-    
-    	} else {
-		document.getElementById("count").innerHTML= caffeineIntake + " mg of caffeine was consumed";
-
-		}
-	
 	});
     
     $("#sbucks").click(function () {
     caffeineIntake = caffeineIntake + 146;
-    
-    if(caffeineIntake >= 10000){
-		
-		 document.getElementById("count").innerHTML= caffeineIntake + "mg of caffeine was consumed...how are you not dead?";
-    	$("#count").css("color", "red");
-		
-		} else if(caffeineIntake >= 1000){
-		
-		 document.getElementById("count").innerHTML= caffeineIntake + "mg of caffeine was consumed...no seriously stop";
-    	$("#count").css("color", "red");
-		
-		} else if(caffeineIntake >= 400){
-    	
-        document.getElementById("count").innerHTML= caffeineIntake + "mg of caffeine was consumed...but you should stop now";
-    	$("#count").css("color", "red");
-    
-   	 } else {
-		
-		console.log("SBUX!");
-		document.getElementById("count").innerHTML= caffeineIntake + "mg of caffeine was consumed";
-     }
+    caffeineCheck(caffeineIntake);
     
     });
     
     $("#mountaindew").click(function () {
     caffeineIntake = caffeineIntake + 54;
-    
-    if(caffeineIntake >= 10000){
-		
-		 document.getElementById("count").innerHTML= caffeineIntake + "mg of caffeine was consumed...how are you not dead?";
-    	$("#count").css("color", "red");
-		
-		} else if(caffeineIntake >= 1000){
-		
-		 document.getElementById("count").innerHTML= caffeineIntake + "mg of caffeine was consumed...no seriously stop";
-    	$("#count").css("color", "red");
-		
-		} else if(caffeineIntake >= 400){
-    
-        document.getElementById("count").innerHTML= caffeineIntake + "mg of caffeine was consumed...but you should stop now";
-    	$("#count").css("color", "red");
-    
-    	} else {
-		
-		console.log("DO THE DEW");
-		document.getElementById("count").innerHTML= caffeineIntake + "mg of caffeine was consumed";
-
-   } });
+    caffeineCheck(caffeineIntake);
+    });
     
     $("#pepsi").click(function () {
     caffeineIntake = caffeineIntake + 38;
+	caffeineCheck(caffeineIntake);
 
-    if(caffeineIntake >= 10000){
-
-		 document.getElementById("count").innerHTML= caffeineIntake + "mg of caffeine was consumed...how are you not dead?";
-    	$("#count").css("color", "red");
-		
-		} else if(caffeineIntake >= 1000){
-		
-		 document.getElementById("count").innerHTML= caffeineIntake + "mg of caffeine was consumed...no seriously stop";
-    	$("#count").css("color", "red");
-		
-		} else if(caffeineIntake >= 400){
-        document.getElementById("count").innerHTML= caffeineIntake + "mg of caffeine was consumed...but you should stop now";
-    	$("#count").css("color", "red");
-    
-    	} else {
-		
-		console.log("PEPSI");
-		document.getElementById("count").innerHTML= caffeineIntake + "mg of caffeine was consumed";
-
-    }});
+	});
     
     $("#coke").click(function () {
     caffeineIntake = caffeineIntake + 35;
-    
-    if(caffeineIntake >= 10000){
-		
-		 document.getElementById("count").innerHTML= caffeineIntake + "mg of caffeine was consumed...how are you not dead?";
-    	$("#count").css("color", "red");
-		
-		} else if(caffeineIntake >= 1000){
-		
-		 document.getElementById("count").innerHTML= caffeineIntake + "mg of caffeine was consumed...no seriously stop";
-    	$("#count").css("color", "red");
-		
-		} else if(caffeineIntake >= 400){
-    
-        document.getElementById("count").innerHTML= caffeineIntake + "mg of caffeine was consumed...but you should stop now";
-    	$("#count").css("color", "red");
-    
-    	} else {
-		
-		console.log("COKE");
-		document.getElementById("count").innerHTML= caffeineIntake + "mg of caffeine was consumed";
-
-   } });
+    caffeineCheck(caffeineIntake);
+    });
     
     $("#five").click(function () {
     caffeineIntake = caffeineIntake + 208;
-   
-   if(caffeineIntake >= 10000){
-		
-		 document.getElementById("count").innerHTML= caffeineIntake + "mg of caffeine was consumed...how are you not dead?";
-    	$("#count").css("color", "red");
-		
-		} else if(caffeineIntake >= 1000){
-		
-		 document.getElementById("count").innerHTML= caffeineIntake + "mg of caffeine was consumed...no seriously stop";
-    	$("#count").css("color", "red");
-		
-		} else if(caffeineIntake >= 400){
-    
-        document.getElementById("count").innerHTML= caffeineIntake + "mg of caffeine was consumed...but you should stop now";
-    	$("#count").css("color", "red");
-    
-    	} else {
-		
-		console.log("%!");
-	    document.getElementById("count").innerHTML= caffeineIntake + "mg of caffeine was consumed";
-
-   } });
+   caffeineCheck(caffeineIntake); 
+   });
     
     $("#monster").click(function () {
     caffeineIntake = caffeineIntake + 160;
-    if(caffeineIntake >= 10000){
-		
-		 document.getElementById("count").innerHTML= caffeineIntake + "mg of caffeine was consumed...how are you not dead?";
-    	$("#count").css("color", "red");
-		
-		} else if(caffeineIntake >= 1000){
-		
-		 document.getElementById("count").innerHTML= caffeineIntake + "mg of caffeine was consumed...no seriously stop";
-    	$("#count").css("color", "red");
-		
-		} else if(caffeineIntake >= 400){
-    
-        document.getElementById("count").innerHTML= caffeineIntake + "mg of caffeine was consumed...but you should stop now";
-    	$("#count").css("color", "red");
-    
-   	 	} else {
-		
-		console.log("everybody know im a muthafckin monster!");
-	    document.getElementById("count").innerHTML= caffeineIntake + "mg of caffeine was consumed";
-
-   } });
+   	caffeineCheck(caffeineIntake); 
+   	});
     
     $("#redbull").click(function () {
     caffeineIntake = caffeineIntake + 80;
     
-    if(caffeineIntake >= 10000){
-		
-		 document.getElementById("count").innerHTML= caffeineIntake + "mg of caffeine was consumed...how are you not dead?";
-    	$("#count").css("color", "red");
-		
-		} else if(caffeineIntake >= 1000){
-		
-		 document.getElementById("count").innerHTML= caffeineIntake + "mg of caffeine was consumed...no seriously stop";
-    	$("#count").css("color", "red");
-		
-		} else if(caffeineIntake >= 400){
-    
-        document.getElementById("count").innerHTML= caffeineIntake + "mg of caffeine was consumed...but you should stop now";
-    	$("#count").css("color", "red");
-    
-    	} else {
-		
-		console.log("it gives you wings!!");
-	    document.getElementById("count").innerHTML= caffeineIntake + "mg of caffeine was consumed";
-
-   } });
+    caffeineCheck(caffeineIntake);
+     });
     
 }
