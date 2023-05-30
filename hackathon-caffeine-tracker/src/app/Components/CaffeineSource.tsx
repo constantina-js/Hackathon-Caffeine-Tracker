8b8b3157b0afc1b2a42df30934c20598dc7723f8
Constantina "Connie" Scoullis <scoullisconstantina@gmail.com>
2023-05-30 13:10:00 -0700

    Basic functionality works in Next.js version

--- hackathon-caffeine-tracker/src/app/Components/CaffeineSource.tsx
+++ hackathon-caffeine-tracker/src/app/Components/CaffeineSource.tsx
@@ -1,19 +1,30 @@
 "use client"
 import { Component, ReactNode } from "react";
+import css from '../styles.module.css';
+import Image from 'next/image';
 
 interface CaffeineProps {
-    sourceName: string;
-    amountOfCaffeine: number;
+    name: string;
+    amount: number;
+    servingAmount: number;
+    onClickFunc: Function;
 }
 
 export class CaffeineSource extends Component<CaffeineProps>{
     constructor(props: CaffeineProps){
-        super(props)
+        super(props);
     }
 
     render(): ReactNode {
+        const { name, amount, servingAmount, onClickFunc } = this.props;
         return (
-            <div>
+
+            <div className={css.caffeineSource} id={name}>
+                <Image src={'/../public/caffeine-molecule.jpg'} width={140} height={140}/>
+                <span>{name}</span>
+                <br/>
+                <span>{amount} / {servingAmount}mg</span>
+                <button onClick={() => {onClickFunc(amount)}}>Select</button>
             </div>
         ) 
     }
--- hackathon-caffeine-tracker/src/app/page.tsx
+++ hackathon-caffeine-tracker/src/app/page.tsx
@@ -1,18 +1,41 @@
 "use client"
 import './styles.css'
 import css from './styles.module.css';
+import { useState } from 'react';
 import { Counter } from './Components/Counter';
 import { CaffeineSource } from './Components/CaffeineSource';
 import data from './caffeine-sources.json'
 
 export default function Home() {
 
-  let counterProps = {
-    totalCaffeine: 0,
-    message:''
-  };
+  const [totalCaffeine, setCaffeine] = useState(0);
+  const [message, setMessage] = useState(``)
 
-  
+  let dataRows = [[]];
+  let dataRowsIndex = 0;
+  let dataCount = 0;
+  for(let i=0; i<data.length; i++){
+    if(dataCount < 3) {
+      dataRows[dataRowsIndex].push(data[i])
+      dataCount++;
+    } else {
+      dataRowsIndex++;
+      dataRows[dataRowsIndex] = [];
+      dataCount = 0;
+    }
+  }
+
+  const onCaffeineClick = (addedAmount:number) => {
+    setCaffeine(totalCaffeine + addedAmount);
+    return;
+  }
+
+  const resetCounter = () => {
+    setCaffeine(0);
+    return;
+  }
+
+  const counterProps = {totalCaffeine, message}
 
   return (
     <main>
@@ -20,8 +43,27 @@ export default function Home() {
       <h2>Keep track of your caffeine consumption as you hack!</h2>
       <Counter {...counterProps}></Counter>
       <div className={css.buttonContainer}>
-
+        <table>
+        {
+          dataRows.map(x => {
+           return (
+           <tr>
+            {x.map((y) => {
+                const caffeineSourceProps = {...y, onClickFunc: onCaffeineClick}
+                return (
+                  <td>
+                    <CaffeineSource {...caffeineSourceProps}></CaffeineSource>
+                  </td>
+                )
+            })}
+           </tr>
+           )
+          })
+        }
+        </table>
       </div>
+
+      <button className={css.resetButton} onClick={()=> {resetCounter()}}>Reset Counter</button>
     </main>
   )
 }
